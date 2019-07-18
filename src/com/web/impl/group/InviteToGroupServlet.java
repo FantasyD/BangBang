989d1dad601d6f726ffdfc81b5a2a3e1dd73146c
Fantasy <2449125662@qq.com>
2019-07-18 10:56:23 +0800

    提出导航栏

--- src/com/web/impl/group/InviteToGroupServlet.java
+++ src/com/web/impl/group/InviteToGroupServlet.java
@@ -1,9 +1,5 @@
 package com.web.impl.group;
 
-import com.services.impl.Ae01ServiceImpl;
-import com.services.impl.Ah01ServiceImpl;
-import com.web.support.ControllerSupport;
-
 /**
  * @Description: 邀请其他用户进入群组
  * @author: 宁志豪
@@ -13,7 +9,7 @@ public class InviteToGroupServlet extends GroupServlet
 	@Override
 	public String execute() throws Exception
 	{
-		this.update("sendEmail", "邀请");
+		this.update("inviteGroup", "邀请");
 		this.savePageInstance();
 		this.savePageData();
 		return "group";
